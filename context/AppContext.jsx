04cd3a83e62460dbfbf052ae03b2a99f39ec7c29
Iftee97 <tawsifmasrur@gmail.com>
2023-07-18 14:03:59 +0600

    removed unnecessary code

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -7,13 +7,7 @@ export const AppContext = createContext()
 export default function AppContextProvider({ children }) {
   const [showSidebar, setShowSidebar] = useState(false)
   const [showUserDropdown, setShowUserDropdown] = useState(false)
-  const [categories, setCategories] = useState([])
-  const [categoriesLoading, setCategoriesLoading] = useState(false)
   const router = useRouter()
-
-  // useEffect(() => {
-  //   getCategories()
-  // }, [])
 
   // set showUserDropdown to false when route changes
   useEffect(() => {
@@ -24,26 +18,12 @@ export default function AppContextProvider({ children }) {
     setShowSidebar(!showSidebar)
   }
 
-  // async function getCategories() {
-  //   try {
-  //     setCategoriesLoading(true)
-  //     const response = await axios.get('https://iftee97.github.io/categories.json')
-  //     setCategories(response.data)
-  //   } catch (error) {
-  //     console.log(error)
-  //   } finally {
-  //     setCategoriesLoading(false)
-  //   }
-  // }
-
   return (
     <AppContext.Provider
       value={{
         showSidebar,
         setShowSidebar,
         toggleSidebar,
-        categories,
-        categoriesLoading,
         showUserDropdown,
         setShowUserDropdown,
       }}
