43a3402ce79f6e16143142ad0690c9ec9b7bc2f0
Tawsif Haque <68259102+Iftee97@users.noreply.github.com>
2023-12-25 13:36:09 +0600

    the same API that handles pagination

--- pages/api/products/get-all-products.js
+++ pages/api/products/get-all-products.js
@@ -10,3 +10,38 @@ export default async function handler(req, res) {
     res.status(500).json({ message: error.message });
   }
 }
+
+// // the same API that handles pagination
+/* 
+import Product from "@/models/product";
+import { connectToDb } from "@/utils/db";
+
+export default async function handler(req, res) {
+  try {
+    await connectToDb();
+
+    // Extract query parameters for pagination
+    const { page = 1, limit = 10 } = req.query;
+
+    // Convert page and limit to integers
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+
+    // Validate page and limit values
+    if (isNaN(pageNumber) || isNaN(limitNumber) || pageNumber < 1 || limitNumber < 1) {
+      return res.status(400).json({ message: "Invalid page or limit parameters" });
+    }
+
+    // Calculate the skip value for MongoDB query
+    const skip = (pageNumber - 1) * limitNumber;
+
+    // Fetch products with pagination
+    const products = await Product.find({}).skip(skip).limit(limitNumber);
+
+    res.status(200).json({ products, page: pageNumber, limit: limitNumber });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+*/
