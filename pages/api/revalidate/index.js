21249f31d8cf8ef304d967982de8378c5d61762d
Tawsif Haque <68259102+Iftee97@users.noreply.github.com>
2023-12-25 13:30:46 +0600

    add example for on-demand revalidation api

--- pages/api/revalidate/index.js
+++ pages/api/revalidate/index.js
@@ -16,3 +16,40 @@ export default async function handler(req, res) {
     });
   }
 }
+
+// // on-demand revalidation for next js page router. but the api must be from a separate api, not next js api
+/* 
+export default async function handler(req, res) {
+  // check for the POST request
+  if (req.method !== "POST") {
+    return res
+      .status(400)
+      .json({ error: "Invalid HTTP method. Only POST requests are allowed." });
+  }
+
+  // check for the secret token
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  try {
+    // check that body is not empty
+    const body = req.body;
+    if (!body) {
+      res.status(400).send("Bad request (no body)");
+      return;
+    }
+
+    // get the slug to revalidate from body
+    const slugToRevalidate = body.slugToRevalidate;
+    if (slugToRevalidate) {
+      await res.unstable_revalidate(/blog/${slugToRevalidate});
+      return res.json({ revalidated: true });
+    }
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send("Error revalidating");
+  }
+}
+*/
