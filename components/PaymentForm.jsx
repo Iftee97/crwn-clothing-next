deeae5c64afe6d5fb0f70f510e558a5e8aed1073
Iftee97 <tawsifhaque97@gmail.com>
2024-03-28 15:07:08 +0600

    Fix JWT token decoding issue and handle payment success and cancellation

--- components/PaymentForm.jsx
+++ components/PaymentForm.jsx
@@ -2,111 +2,67 @@ import { useRouter } from "next/router";
 import { useState, useContext } from "react";
 import { CartContext } from "@/context/CartContext";
 import { AuthContext } from "@/context/AuthContext";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { toast } from "react-toastify";
-import axios from "axios";
 import Cookies from "js-cookie";
 
 export default function PaymentForm() {
   const router = useRouter();
   const { user } = useContext(AuthContext);
   const { cartItems, getCartTotal, clearCart } = useContext(CartContext);
-  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const stripe = useStripe();
-  const elements = useElements();
   const amount = getCartTotal();
 
-  async function paymentHandler(e) {
+  async function stripeRedirectCheckout(e) {
     e.preventDefault();
-    if (!stripe || !elements) {
-      return;
-    }
+
     if (!user._id) {
       router.push("/sign-in");
       toast.info("You must be logged in to make a payment.");
       return;
     }
+
     try {
-      setIsProcessingPayment(true);
-      const response = await fetch("/api/stripe/create-payment-intent", {
+      setIsLoading(true);
+      const response = await fetch("/api/stripe/checkout", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${Cookies.get("token")}`,
+        },
         body: JSON.stringify({
-          amount: amount * 100,
+          items: cartItems,
+          total: amount,
         }),
       });
-      const data = await response.json();
-      // console.log('data: >>>>>>>>>', data)
-      const clientSecret = data.client_secret;
-      const { paymentIntent, error } = await stripe.confirmCardPayment(
-        clientSecret,
-        {
-          payment_method: {
-            card: elements.getElement(CardElement),
-            billing_details: {
-              name: user?.username || "guest",
-            },
-          },
-        }
-      );
-      // console.log('paymentIntent: >>>>>>>>>', paymentIntent)
-      if (error) {
-        throw new Error(error.message);
-      } else if (paymentIntent.status === "succeeded") {
-        toast.success("Payment Successful!");
-
-        // Create an order in the database
-        const orderResponse = await axios.post(
-          "/api/orders/create-order",
-          {
-            user: user._id,
-            items: cartItems,
-            total: amount,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${Cookies.get("token")}`,
-            },
-          }
-        );
-        if (orderResponse.status === 201) {
-          toast.success("Order placed successfully!");
-        } else {
-          throw new Error(
-            "There was an error processing your order. Please try again."
-          );
-        }
-
-        clearCart();
-        router.push("/");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      } else {
+        const { redirectUrl } = await response.json();
+        router.push(redirectUrl);
+        // clearCart();
+        // toast.success("Payment successful");
       }
     } catch (error) {
-      console.log("error: >>>>>>>>>", error);
-      toast.error(error.message);
+      console.log("error: >>>>>>", error);
+      setIsLoading(false);
     } finally {
-      setIsProcessingPayment(false);
+      setIsLoading(false);
     }
   }
 
   return (
-    <div className="payment-form-container flex justify-center lg:justify-end mt-8 w-full">
-      <form
-        className="payment-form min-w-0 lg:min-w-[500px]"
-        onSubmit={paymentHandler}
-      >
-        <h2 className="font-medium text-[22px]">Credit Card Payment:</h2>
-        <div className="my-6 border border-gray-300 lg:border-none p-3 lg:p-0">
-          <CardElement />
-        </div>
+    <div className="flex justify-center lg:justify-end mt-8 w-full">
+      <form onSubmit={stripeRedirectCheckout}>
         <button
           type="submit"
-          className={`
-          bg-black hover:bg-gray-800 text-white py-2 px-4 rounded 
-            ${isProcessingPayment && "opacity-50 cursor-not-allowed"}
-          `}
-          disabled={isProcessingPayment}
+          disabled={isLoading}
+          className={`bg-black hover:bg-gray-800 text-white py-2 px-4 rounded ${
+            isLoading &&
+            "opacity-50 cursor-not-allowed bg-gray-500 hover:bg-gray-500 "
+          }`}
         >
-          {isProcessingPayment ? "Processing..." : "Pay Now"}
+          {isLoading ? "loading..." : "pay now"}
         </button>
       </form>
     </div>
---
+++ pages/api/stripe/checkout.js
@@ -0,0 +1,64 @@
+import Stripe from "stripe";
+import Order from "@/models/order";
+import { connectToDb } from "@/utils/db";
+import jwt from "jsonwebtoken";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY, {
+  apiVersion: "2022-11-15",
+});
+
+export default async function handler(req, res) {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "Not Authorized" });
+  }
+
+  const token = authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+  console.log("decodedToken: >>>>>>>", decodedToken);
+  if (!decodedToken || !decodedToken.userId) {
+    return res.status(401).json({ error: "Invalid Token" });
+  }
+
+  const { items, total } = req.body;
+  console.log({ items, total });
+
+  const lineItems = items.map((item) => ({
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: item.title,
+        images: [item.imageUrl],
+      },
+      unit_amount: item.price * 100,
+    },
+    quantity: item.quantity,
+  }));
+
+  try {
+    // const order = await Order.create({
+    //   user: decodedToken.userId,
+    //   items,
+    //   total,
+    // });
+    // console.log("created order: >>>>>>>>", order);
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${process.env.NEXT_PUBLIC_URL}/checkout?success=1`,
+      cancel_url: `${process.env.NEXT_PUBLIC_URL}/checkout?canceled=1`,
+    });
+    console.log("session: >>>>>>>>", session);
+
+    res.status(200).json({
+      message: "Payment successful",
+      redirectUrl: session.url,
+    });
+  } catch (error) {
+    console.log("error: >>>>>>>>", error);
+    res
+      .status(400)
+      .json({ message: "Error: could not process payment and create order" });
+  }
+}
