4db979d11ea780c5c020bc9339666788b4d76433
Iftee97 <tawsifhaque97@gmail.com>
2024-04-10 01:22:46 +0600

    remove unnecessary componentization

--- pages/shop/index.jsx
+++ pages/shop/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import CategoryPreview from "@/components/CategoryPreview";
-// import getBase64 from "@/utils/getLocalBase64";
+import Link from "next/link";
+import ProductCard from "@/components/ProductCard";
 
 export default function Shop({ categories }) {
   return (
@@ -10,8 +10,17 @@ export default function Shop({ categories }) {
       </Head>
 
       <div className="flex flex-col gap-6">
-        {categories.map((category) => (
-          <CategoryPreview key={category._id} category={category} />
+        {categories.map(({ title, items: products }) => (
+          <div className="flex flex-col mb-[18px] lg:mb-[36px]">
+            <h2 className="text-[28px] mb-3">
+              <Link href={`/shop/${title}`}>{title.toUpperCase()}</Link>
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-x-[16px] lg:grid-cols-4 gap-y-[12px] lg:gap-y-[0] gap-x-[0] lg:gap-x-[20px]">
+              {products?.slice(0, 4).map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+            </div>
+          </div>
         ))}
       </div>
     </>
@@ -20,17 +29,9 @@ export default function Shop({ categories }) {
 
 export async function getStaticProps() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/categories/get-categories-with-items`
+    `${process.env.NEXT_PUBLIC_API_URL}/categories/get-categories-with-items`,
   );
   const { categories } = await response.json();
-
-  // // Iterate through items array in categories and add blurDataURL property using item.imageUrl
-  // for (const category of categories) {
-  //   for (const item of category.items) {
-  //     const blurDataURL = await getBase64(item.imageUrl);
-  //     item.blurDataURL = blurDataURL;
-  //   }
-  // }
 
   return {
     props: {
